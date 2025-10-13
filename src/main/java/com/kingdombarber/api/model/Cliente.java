91d1370dc772fe04143c4b234b918ddc0534ccc8
Juan Rivera24 <juanmrivera0424@gmail.com>
2025-10-13 11:23:33 -0500

    despliegue

---
+++ src/main/java/com/kingdombarber/api/model/Cliente.java
@@ -0,0 +1,22 @@
+// Archivo: Cliente.java
+package com.kingdombarber.api.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity @Table(name = "CLIENTES") @Getter @Setter
+public class Cliente {
+    @Id @Column(name = "ID_Cliente")
+    private Long id;
+    @Column(name = "Nombre_Cliente")
+    private String nombreCliente;
+    @Column(name = "Apellido_Cliente")
+    private String apellidoCliente;
+    @Column(name = "Telefono")
+    private String telefono;
+    @Column(name = "Email")
+    private String email;
+}
